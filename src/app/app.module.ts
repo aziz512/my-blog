c4726b8fd50bac0e4cf8bcaeaf4c3dbafa5a6671
Aziz Yokubjonov <aziz.yokubjonov@gmail.com>
2019-08-28 22:47:04 -0400

    move out firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
-import { firebaseConfig } from 'src/environments/environment';
+import { firebaseConfig } from 'src/app/firebase-config';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,16 +6,6 @@ export const environment = {
   production: false
 };
 
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCnIwmA6c7xGbcsODUCoeYineiF_qCc_gA',
-  authDomain: 'my-blog-5360d.firebaseapp.com',
-  databaseURL: 'https://my-blog-5360d.firebaseio.com',
-  projectId: 'my-blog-5360d',
-  storageBucket: 'my-blog-5360d.appspot.com',
-  messagingSenderId: '773224394070',
-  appId: '1:773224394070:web:8af40d8dfb195cc8'
-};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
