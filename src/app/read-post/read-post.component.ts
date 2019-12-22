dfa3c5be5e5cdee12f48e43dcf0ae971a4317a30
Aziz Yokubjonov <aziz.yokubjonov@gmail.com>
2019-12-22 17:50:57 -0500

    fix navigation problem

--- src/app/read-post/read-post.component.ts
+++ src/app/read-post/read-post.component.ts
@@ -20,26 +20,27 @@ export class ReadPostComponent implements OnInit {
   }
 
   async ngOnInit() {
-    const currentRoute = this.route.snapshot;
+    this.route.url.subscribe(async (url) => {
+      const currentRoute = this.route.snapshot;
+      let path: string;
+      this.isSitePage = url[0].path !== 'post';
+      if (this.isSitePage) {
+        path = 'pages';
+      }
 
-    let path: string;
-    this.isSitePage = currentRoute.url[0].path !== 'post';
-    if (this.isSitePage) {
-      path = 'pages';
-    }
+      const slug = currentRoute.paramMap.get('slug');
+      const dataKey = makeStateKey(`post/${slug}/${path}`);
+      const $dataSource = this.firebaseService.getPostBySlug(slug, path);
+      this.post$ = this.firebaseService.getCachedObservable($dataSource, dataKey);
+      const post = await this.post$.toPromise();
+      if (!post) {
+        this.router.navigate(['404']);
+        return;
+      }
 
-    const slug = currentRoute.paramMap.get('slug');
-    const dataKey = makeStateKey(`post/${slug}/${path}`);
-    const $dataSource = this.firebaseService.getPostBySlug(slug, path);
-    this.post$ = this.firebaseService.getCachedObservable($dataSource, dataKey);
-    const post = await this.post$.toPromise();
-    if (!post) {
-      this.router.navigate(['404']);
-      return;
-    }
-
-    this.titleService.setTitle(post.title + ' - Aziz Yokubjonov');
-    this.comments$ = !this.isSitePage ? this.firebaseService.getCommentsForPost(post.id) : null;
+      this.titleService.setTitle(post.title + ' - Aziz Yokubjonov');
+      this.comments$ = !this.isSitePage ? this.firebaseService.getCommentsForPost(post.id) : null;
+    });
   }
 
 }
